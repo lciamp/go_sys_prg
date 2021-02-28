8a375d6e0b00815c5415ae117b0a97caff75d3fb
lciamp <lou.ciampanelli@equinox.com>
2021-02-28 18:44:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# go_sys_prg
---
+++ cla.go
@@ -0,0 +1,14 @@
+// command line args
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	arguments := os.Args
+	for i := 0; i < len(arguments); i++ {
+		fmt.Println(arguments[i])
+	}
+}
