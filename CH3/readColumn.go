540ad5fa8f94badd84d5276c9316c5881239dd6c
lciamp <l.ciamp@yahoo.com>
2021-03-07 21:08:32 -0500

    reading columns using strings.Fields

--- CH3/readColumn.go
+++ CH3/readColumn.go
@@ -11,7 +11,7 @@ func main() {
 	s[1] = "11 12 13 14 15 16"
 	s[2] = "-1 2 -3 -4 -5 6"
 
-	column := 2
+	column := 3
 
 	for i := 0; i < len(s); i++ {
 		data := strings.Fields(s[i]) // Fields splits a string based on whitespace (like split in python)
